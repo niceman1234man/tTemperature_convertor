1421c021c65b317bef737adddadf7e299745f0ae
niceman1234man <yihunietarekegn18@gmail.com>
2024-02-14 22:08:11 +0300

    temperature convertor

---
+++ jspart.js
@@ -0,0 +1,18 @@
+const input = document.getElementById("input");
+const radio1 = document.getElementById("radio1");
+const radio2 = document.getElementById("radio2");
+const p = document.getElementById("p");
+
+function convert() {
+    let temp;
+    
+    if (radio1.checked) {
+        temp = Number(input.value);
+        temp = (9 / 5) * temp + 32;
+        p.textContent = `${temp}°F`;
+    } else if (radio2.checked) {
+        temp = Number(input.value);
+        temp = (5 / 9) * (temp - 32);
+        p.innerHTML = `${temp}°C`;
+    }
+}
